08e9b6b77e96d9dce81b0392eab56bdb25273a5d
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-07 09:50:02 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ross Phillips on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
