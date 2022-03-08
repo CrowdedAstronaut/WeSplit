3276060074d434b85eab97737fd77e92c2658352
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-08 10:44:46 -0600

    Reading text from the user with TextField

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
 		Form {
 			Section {
 				TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-					.keyboardType(.decimalPad)
+					.keyboardType(.numberPad)
 			}
 			
 			Section {
