166bf7abf6c962ca5c034e3c11a0811d972bd03a
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-08 12:10:42 -0600

    Hiding the keyboard

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+	@FocusState private var amountIsFocused: Bool
 	@State private var checkAmount = 0.0
 	@State private var numberOfPeople = 2
 	@State private var tipPercentage = 20
@@ -20,7 +21,9 @@ struct ContentView: View {
 		let tipValue = checkAmount / 100 * tipSelection
 		let grandTotal = checkAmount + tipValue
 		let amountPerPerson = grandTotal / peopleCount
-		return amountPerPerson	}
+		return amountPerPerson
+		
+	}
 	
 	var body: some View {
 		NavigationView {
@@ -28,6 +31,7 @@ struct ContentView: View {
 				Section {
 					TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
 						.keyboardType(.numberPad)
+						.focused($amountIsFocused)
 					
 					Picker("Number of people", selection: $numberOfPeople) {
 						ForEach(2 ..< 100) {
@@ -51,6 +55,14 @@ struct ContentView: View {
 					Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
 				}
 				.navigationTitle("WeSplit")
+				.toolbar {
+					ToolbarItemGroup(placement: .keyboard) {
+						Button("Done") {
+							amountIsFocused = false
+						}
+					}
+				}
+				
 			}
 		}
 	}
